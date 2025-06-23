8d0f24f5c5a5b5a3755c6e08f4c619ecec46e705
Ben Lackey <ben.lackey@outlook.com>
2025-06-23 13:04:47 +0200

    login works.  but API is ancient (3.1 when current is 23)

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 import json
+import http
 import facebook
 
 with open('secrets.json') as f:
     secrets = json.load(f)
 
+conn = http.client.HTTPSConnection('graph.facebook.com')
+conn.request(method = 'GET', 
+            url = '/oauth/access_token?client_id=' + secrets['app_id'] + '&client_secret=' + secrets['app_secret'] + '&grant_type=client_credentials',
+            headers= {'User-Agent':'curl/7.68.0'} 
+            )
+response = conn.getresponse()
+print(response.status, response.reason)
+data = response.read()
+conn.close()
 
+jsondata = json.loads(data)
+access_token = jsondata['access_token']
 
-#graph = facebook.GraphAPI(app_secret=secrets['app_secret'], version="23.0")
-graph = facebook.GraphAPI(secrets['access_token'], version="23.0")
+graph = facebook.GraphAPI(access_token, version="3.1")
 
 profile = graph.get_object('me',fields='first_name,last_name,location,link,email')  
 print(json.dumps(profile, indent=5))
 
+'''
 events = graph.request('type=event&limit=10000')
 print(events)
+'''
